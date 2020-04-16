b2f9dea9db977e6f3a7f8a94a24d917073bf38dc
Daniil <brayzer208@gmail.com>
2020-04-16 13:44:07 +0300

    Home_work

---
+++ home_work.js
@@ -0,0 +1,22 @@
+'use strict';
+
+
+let money = prompt('Ваш бюджет?' );
+let time = prompt('Введите дату в формате YYYY-MM-DD' );
+
+let a = prompt('Введите обязательную статью расходов в этом месяце', );
+let b = prompt('Во сколько обойдется?', );
+let appData = {
+    moneyData: money,
+    timeData: time,
+    expenses : {},
+    optionalExpenses: {
+        question: a,
+        answer: b,
+    },
+    income : [],
+    saving: false,
+};
+
+console.log(appData.optionalExpenses);
+alert(appData.optionalExpenses.question + " : " + appData.optionalExpenses.answer);
