838cc34750bb54af10945719b5d868698ae2f110
Daniil <brayzer208@gamil.com>
2020-05-15 16:40:04 +0300

    new version

--- home_work.js
+++ home_work.js
@@ -1,96 +1,22 @@
 'use strict';
 
-let money;
-let time;
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели', '');
 
 
-function start() {
-    money = +prompt('Ваш бюджет?', '');
-    time = prompt('Введите дату в формате YYYY-MM-DD', '');
-
-    while(isNaN(money) || money == '' || money == null) {
-        money = +prompt('Ваш бюджет?', '');
-    }
-}
-start();
-
-
-let appData = {
-    budget: money,
-    expenses: {},
-    optionalExpenses: {},
-    income: [],
-    timeData: time,
-    savings: true,
-    chooseExpenses: function() {
-        for(let i = 0; i < 2; i++) {
-            let a = prompt('Введите обязательную статью расходов в этом месяце', ""),
-                b = prompt('Во сколько обойдется?', "");
-        
-            
-            if( (typeof(a) ) === 'string' && typeof(a) != null && typeof(b) != null && a != "" && b != "" && a.length < 50) {
-                console.log('done!');
-                appData.expenses[a] = b;
-            } else {
-                i--;
-            }
-        }
-    },
-    detectDayBudget: function() {
-        appData.moneyPerDay = (appData.budget / 30).toFixed();
-        alert("Ежедневный бюджет: " + appData.moneyPerDay + " рублей");
-    },
-    detectLevel: function() {
-        if (appData.moneyPerDay < 100) {
-            alert ("Это минимальный уровень достатка!");
-        } else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
-            alert ("Это средний уровень достатка!");
-        } else if (appData.moneyPerDay > 2000) {
-            alert ("Это высокий уровень достатка!");
-        } else {
-            alert ("Произошла ошибка");
-        }
-    },
-    checkSavings: function() {
-        if(appData.savings == true) {
-            let save = +prompt('Какова сумма накоплений', '');
-            let percent = +prompt('Под какой процент', '');
-    
-            appData.mounthIncome = save / 100 / 12 * percent;
-            alert('Доход в месяц с вашего депозита ' + appData.mounthIncome);
-        }
-    },
-    chooseOptExpenses: function() {
-        for( let i = 0; i < 3; i++) {
-            let a = prompt('Номер статьи необязательных расходов', '');
-            let b = prompt('Сколько тратите?', '');
-    
-    
-            if( (typeof(a) ) === 'string' && typeof(a) != null && typeof(b) != null && a != "" && b != "" && a.length < 50) {
-                appData.optionalExpenses[a] = b;
-            } else {
-                i--;
-            }
-        }
-    },
-    choseIncome: function() {
-        for(let i = 0; i < 1; i++) {
-            let items = prompt('Что принесет дополнительный доход (Перечислите через запятую)','');
-            appData.income = items.split(', ');
-            if(typeof(items) === 'string' && typeof(items) != null && items != '') {
-                appData.income.push = prompt('Может какие то еще дохоты?','');
-                appData.income.sort();
-                appData.income.forEach(function(items, i) {
-                alert('Способы доп.заработка :' + ' ' + (i+1) +' : ' + items);
-            }); 
-            } else {
-                alert("Не верно введенные данные, попробуйте еще раз!");
-                i--;
-            }
-             
-        }     
-    }
+const persnalMovieDB = {
+    county: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    private: false,
 };
-for (let key in appData) {
-    console.log('Наша программа включает в себя данные : ' + key);
+
+for (let i = 0; i < 2; i++) {
+    let lastFilm = prompt('Какой фильм вы посмотрели последним?', '');
+    let assessment = prompt('На сколько вы его оцените по 5 бальной шкале?', '');
+
+    persnalMovieDB.movies[lastFilm] = assessment;
 }
+
+
+console.log(persnalMovieDB);
